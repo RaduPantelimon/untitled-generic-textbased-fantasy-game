7054f06ba11684b1f3eaeab35acb28b680ac1413
RaduPantelimon <radu.bucur2@gmail.com>
2023-01-27 23:19:31 +0200

    add dagger class

---
+++ GenericTextBasedRPG/Equipment/Weapons/Dagger.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPGUtilities.Equipment.Weapons
+{
+    internal class Dagger : Weapon
+    {
+        internal Dagger(int minDamage, int maxDamage)
+            : base(minDamage, maxDamage, DamageTypes.Piercing)
+        {
+        }
+    }
+}
