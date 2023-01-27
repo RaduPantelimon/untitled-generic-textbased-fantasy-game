1ec780de34e574b2c58d68320c7d50ae9ed5c228
RaduPantelimon <radu.bucur2@gmail.com>
2023-01-27 20:32:54 +0200

    add randomizer singleton

---
+++ GenericTextBasedRPG/Engine/Randomizer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPGUtilities.Engine
+{
+    //Singleton in current thread
+    public class Randomizer
+    {
+        private static ThreadLocal<Randomizer> instances = new ThreadLocal<Randomizer>(() => new Randomizer());
+
+        public static Randomizer Instance => instances.Value;
+
+        public Random Random { get; }
+
+        private Randomizer()
+        {
+            //TO DO - add seed
+            Random = new Random();
+        }
+
+        public int ThrowDice() => Random.Next(1,7);
+        public double RandomPercentage() => Random.NextDouble();
+    }
+}
