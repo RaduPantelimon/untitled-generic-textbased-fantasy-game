2e6127212b1a0aa192392eb1a34442d2aa271e68
RaduPantelimon <radu.bucur2@gmail.com>
2023-01-27 20:45:18 +0200

    add PhysicalDamageTypes enum

---
+++ GenericTextBasedRPG/Helpers/Enums/PhysicalDamageTypes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPGUtilities
+{
+    [Flags]
+    public enum PhysicalDamageTypes
+    {
+        Piercing = 1,
+        Slashing = 2,
+        Bludgeoning = 4,
+    }
+}
