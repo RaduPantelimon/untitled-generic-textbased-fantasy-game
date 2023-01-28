b687f7236a88ec630dfb7c3d112b2651cffa9b0b
RaduPantelimon <radu.bucur2@gmail.com>
2023-01-28 13:59:23 +0200

    add Hostile Party Base Class

---
+++ GenericTextBasedRPG/Core/Combat/HostileParty.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualBasic;
+using RPGUtilities.Creatures;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPGUtilities.Helpers
+{
+    public class HostileParty<T> : Collection<T> where T: Creature
+    {
+    }
+}
