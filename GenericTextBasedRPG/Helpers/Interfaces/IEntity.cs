4bc0d72dad79250dc53d8c67bce3d90269641aff
RaduPantelimon <radu.bucur2@gmail.com>
2023-02-03 22:35:58 +0200

    rename INameable as IEntity and add INameable Interface with just name

---
+++ GenericTextBasedRPG/Helpers/Interfaces/IEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericRPG.Helpers.Interfaces
+{
+    public interface IEntity: INameable
+    {
+        public string DisplayStats();
+    }
+}
---
+++ GenericTextBasedRPG/Helpers/Interfaces/INameable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericRPG
+{
+    //interface used for entities that need to be displayed to the user
+    public interface INameable
+    {
+        public string? Name { get; }
+    }
+}
